4cac732e6a5b257cb9d7f15094c216a610a8c850
Suhas-Shende <suhas.shende19@gmail.com>
2022-06-04 17:05:01 +0530

    Eighth commit

--- student/settings.py
+++ student/settings.py
@@ -28,9 +28,9 @@ django_heroku.settings(locals())
 SECRET_KEY = 'django-insecure--%jiwz-^@t@a&gekar-fz6jtbm$$(@5s8xfghwb_60mz3$*b50'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # import django_heroku
 # django_heroku.settings(locals())
@@ -55,13 +55,14 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
 
     'django.middleware.security.SecurityMiddleware',
-    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     
 
 ]
@@ -84,29 +85,22 @@ TEMPLATES = [
         },
     },
 ]
-
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/static/'
 WSGI_APPLICATION = 'student.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+
 DATABASES = {
     'default': {
          'ENGINE': 'django.db.backends.postgresql',
         # 'NAME': BASE_DIR / 'db.sqlite3',
          'NAME':'studentlogin',
-         'USER':'postgres',
-         'PASSWORD':'admin',
-         'HOST':'localhost',
-         'PORT':'5433',
+         
     }
 }
 
@@ -148,6 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
